5ccae07b06faee93f2be7f6146ab4ae866682c8c
Josh Illes <joshua.t.illes@gmail.com>
2020-03-20 15:39:36 -0400

    Script now gets all URLS, saves them into .yaml file (updated today), then downloads everything from .yaml file

--- app.py
+++ app.py
@@ -1,66 +1,104 @@
 import requests, bs4
-import yaml
+import yaml, os
 
-webpage = "http://www.survivorlibrary.com/library-download"
-yamlFile = "survivor.yaml"
+# webpage = "http://www.survivorlibrary.com/library-download"
+# yamlFile = "survivor.yaml"
 
 
-
-res = requests.get(webpage)
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text, 'lxml')
-
-headers = soup.find_all('td')
-categories = {}
-
-# Get links for the pages of each subcategory
-for header in headers:
-    if header is None:
-        continue
-    try:
-        category = header.a.text
-        if category == "New Additions":
-            continue
-        categories[category] = {}
-        categories[category]["link"] = webpage + header.a['href']
-        categories[category]["books"] = []
-        # "link"="test", "subject"="another test"}
-# webpage + header.a['href'])
-        # links.append(webpage + header.a['href'])
-    except (TypeError, AttributeError) as er:
-        # print("Type Error: %s" %er)
-        pass
-
-# Go through each subcategory link and download it's page to find all the links to the books.
-for i, category in enumerate(categories):
-    if i>2:
-        print("Reached max number of searches")
-        break
-    print("Looking for %s: %s" %(category, categories[category]["link"]))
-    res = requests.get(categories[category]["link"])
+def getLinks(webpage):
+    res = requests.get(webpage)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'lxml')
-    books = soup.find_all('tr')
-    for book in books:
-        if book is None:
+
+    headers = soup.find_all('td')
+    categories = {}
+
+    # Get links for the pages of each subcategory
+    for header in headers:
+        if header is None:
             continue
         try:
-            title, link = book.find_all('td')
-            title = title.text
-            link = webpage + categories[category]["link"] + link.a['href']
-            print("\t%s:\t%s" %(title, link))
-            # title = book.td.text
-            # link = book.td.a['href']
-            # print("Found %s:\t%s" %(title, link))
-            categories[category]["books"].append({"title": title, "link": link})
+            category = header.a.text
+            if category == "New Additions":
+                continue
+            categories[category] = {}
+            categories[category]["link"] = webpage + header.a['href']
+            categories[category]["books"] = []
+
         except (TypeError, AttributeError) as er:
-            # print("Type Error: %s" %er) 
             pass
 
+    # Go through each subcategory link and download it's page to find all the links to the books.
+    for i, category in enumerate(categories):
+        # if i>2:
+        #     print("Reached max number of searches")
+        #     break
+        print("Looking for %s: %s" %(category, categories[category]["link"]))
+        res = requests.get(categories[category]["link"])
+        res.raise_for_status()
+        soup = bs4.BeautifulSoup(res.text, 'lxml')
+        books = soup.find_all('tr')
+        for book in books:
+            if book is None:
+                continue
+            try:
+                title, link = book.find_all('td')
+                title = title.text
+                link =  "http://www.survivorlibrary.com" + link.a['href']
+                print("\t%s:\t%s" %(title, link))
+                categories[category]["books"].append({"title": title, "link": link})
+            except (TypeError, AttributeError, ValueError) as er:
+                pass
+
+
+    return categories
+
+#  Writes all the categories and links to a YAML file so websites don't have to be scrapped everytime
+def writeYamlFile(data, yamlFile):
+    with open(yamlFile, 'w') as writeFile:
+        writeFile.write(yaml.dump(data, default_flow_style=False))
+
+# Gets all information out of yaml file and download all book links
+def downloadFiles(yamlFile):
+    print("importing data from yaml file: %s" %(yamlFile))
+    
+    with open(yamlFile) as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+
+    rootDirectory = "./survivors_library/"
+    if not os.path.exists(rootDirectory):
+        os.mkdir(rootDirectory)
+
+    for category in data:
+        directoryPath = rootDirectory + category.replace(" ", "_").rstrip()
+        if not os.path.exists(directoryPath):
+            print("Making Directory: %s" %(directoryPath))
+            os.mkdir(directoryPath)
+        for i, book in enumerate(data[category]["books"]):
+            # Create filename of pdf to download
+            filename = directoryPath + "/" + data[category]["books"][i]["title"] + ".pdf"
+            
+            # Check if the book is already downloaded, if it is, skip it
+            if os.path.exists(filename):
+                print("Skipping Book: %s, already downloaded" %(data[category]["books"][i]["title"]))
+                continue
+            
+            print("Downloading Book %d of %d in category:  %s" %(i+1, len(data[category]["books"]), category))
+            link = data[category]["books"][i]["link"]
+            r = requests.get(link, allow_redirects=True)
+            
+            # Download and write to file 100KB at a time
+            with open(filename, 'wb') as newFile:
+                for chunk in r.iter_content(100000):
+                    newFile.write(chunk)
 
 
 
-with open(yamlFile, 'w') as writeFile:
-    yaml.dump(categories, writeFile)
+if __name__ == "__main__":
+    webpage = "http://www.survivorlibrary.com/library-download"
+    yamlFile = "survivor.yaml"
 
+    data = getLinks(webpage)
+    writeYamlFile(data, yamlFile)
 
+    downloadFiles(yamlFile)
